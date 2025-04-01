679ef31ef32ced134ce69ba1bebfee96a00b8ff2
JaggerV2026 <123402353+JaggerV2026@users.noreply.github.com>
2025-04-01 16:39:36 -0500

    Added instruction type and mnemonic conversion

--- src/Instruction.java
+++ src/Instruction.java
@@ -22,7 +22,24 @@ public class Instruction {
 
     private void disassemble(String hexString){
         opcode = convertOpcode(hexString);
-        System.out.println(opcode);
+        instructionType = convertInstructionType(hexString);
+        switch(instructionType){
+            case RTYPE:
+                break;
+
+            case ITYPE:
+                mnemonic = convertMnemonic();
+                break;
+
+            case JTYPE:
+                mnemonic = convertMnemonic();
+                break;
+
+            case SYSCALL:
+                mnemonic = convertMnemonic();
+                break;
+        }
+        System.out.println(mnemonic);
     }
 
     //Return the opcode associated with a given hex string.
@@ -30,5 +47,96 @@ public class Instruction {
     private String convertOpcode(String hexString){
         int decimalOpcode = Integer.parseInt(hexString, 16) >> 26;
         return String.format("%02x", decimalOpcode);
+    }
+
+    //Use known opcode to return Instruction Type. Hex string used for syscall
+    private String convertInstructionType(String hexString){
+        //Syscall is always the same, but the opcode is the same as R format
+        if(hexString.equals("0000000c")){
+            return SYSCALL;
+        }
+        //000000 - R Format instructions
+        else if(opcode.equals("00")){
+            return RTYPE;
+        }
+        //Only one j type instruction, and opcode will be 02
+        else if(opcode.equals("02")){
+            return JTYPE;
+        }
+        else{
+            return ITYPE;
+        }
+    }
+
+    //Use known opcode, funct and instructionType to convert mnemonic
+    private String convertMnemonic(){
+        //R type and syscall instructions need to use funct
+        if(instructionType.equals(RTYPE) || instructionType.equals(SYSCALL)){
+            //add - 100000
+            if(funct.equals("20")){
+                return "add";
+            }
+            //and - 100100
+            else if(funct.equals("24")){
+                return "add";
+            }
+            //or - 100101
+            else if(funct.equals("25")){
+                return "or";
+            }
+            //slt - 101010
+            else if(funct.equals("2a")){
+                return "slt";
+            }
+            //sub - 100010
+            else if(funct.equals("22")){
+                return "sub";
+            }
+            //syscall - 001100
+            else if(funct.equals("0c")){
+                return "syscall";
+            }
+        }
+        //everything else uses opcode
+        else{
+            //addiu - 001001
+            if(opcode.equals("09")){
+                return "addiu";
+            }
+            //andi - 001100
+            else if(opcode.equals("0c")){
+                return "andi";
+            }
+            //beq - 000100
+            else if(opcode.equals("04")){
+                return "beq";
+            }
+            //bne - 000101
+            else if(opcode.equals("05")){
+                return "bne";
+            }
+            //j - 000010
+            else if(opcode.equals("02")){
+                return "j";
+            }
+            //lui - 001111
+            else if(opcode.equals("0f")){
+                return "lui";
+            }
+            //lw - 100011
+            else if(opcode.equals("23")){
+                return "lw";
+            }
+            //ori - 001101
+            else if(opcode.equals("0d")){
+                return "ori";
+            }
+            //sw - 101011
+            else if(opcode.equals("2b")){
+                return "sw";
+            }
+        }
+        //No mnemonic was found
+        return "Error";
     }
 }
